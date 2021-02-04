e626419546386c21fe0535f1a8ecd4953d9292f7
llaneristico <angel.estepona@gmail.com>
2021-02-04 21:18:40 +0100

    Modificación desde Ubuntu

---
+++ adios_copia.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP</title>
+</head>
+<body>
+	<h1>Mi página PHP</h1>
+
+	<?php
+	echo "Hasta luego desarrollador";
+	?>
+	
+</body>
+</html>
